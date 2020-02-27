39117988af0d11c598b4dfbd5e62c3fa163907b6
Danielebp <daniele.pecanha@gmail.com>
2020-02-27 15:16:27 -0800

    adding missing commons file

---
+++ commons.h
@@ -0,0 +1,12 @@
+#ifndef __commons_h__
+#define __commons_h__
+
+#include <iostream>
+#include "utils.h"
+
+enum LogLevel { error, info, debug };
+enum RunType { metric, train };
+enum PerfType { cpu, cuda };
+
+#endif
+
